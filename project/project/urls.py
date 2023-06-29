4cb79e8b5fe38d911ecdc3c0ea96adc60d37851b
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-29 16:25:54 +0530

    urls.py ready

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for project project.
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="xxx"),
 ]
