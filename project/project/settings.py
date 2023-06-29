7eeb8dcacd3bbc32740123115f1cec8dc4e3a964
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-29 16:26:56 +0530

    added the app and etc etc to the installed apps in settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
